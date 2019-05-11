32139e84ec74baf7afec089111a09ece6638acc9
Jan Demel <jandemel98@gmail.com>
2019-05-11 12:04:48 +0200

    Overloaded operators == != and setter for X, Y ✅

--- src/Field.h
+++ src/Field.h
@@ -14,6 +14,39 @@ struct Field {
      * Svislá složka
      */
     int y;
-};
 
+    /**
+     * Overriding operator for comparison
+     * @param a
+     * @return
+     */
+    bool operator==(const Field& a) {
+        return (x == a.x && y == a.y);
+    }
+
+    /**
+     * Overriding operator for comparison
+     * @param a
+     * @return
+     */
+    bool operator!=(const Field& a) {
+        return !(x == a.x && y == a.y);
+    }
+
+    /**
+     * Set X argument
+     * @param x
+     */
+    void setX(int x){
+        this->x = x;
+    }
+
+    /**
+     * Set Y argument
+     * @param y
+     */
+    void setY(int y){
+        this->y = y;
+    }
+};
 #endif //CPPPROJECT_FIELD_H
