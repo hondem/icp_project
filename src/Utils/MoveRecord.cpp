ab5dad327876870deb02af4a36236d50f7cf7bf1
Jan Demel <jandemel98@gmail.com>
2019-05-11 22:38:55 +0200

    Added toString() method

--- src/Utils/MoveRecord.cpp
+++ src/Utils/MoveRecord.cpp
@@ -3,7 +3,9 @@
 string MoveRecord::toString(MoveRecord *moveRecord) {
     //moveRecord->figure + moveRecord->source + ((moveRecord->throwAway) ? "x" : "") + moveRecord->target
     stringstream ss;
-    ss << moveRecord->figure << moveRecord->source << ((moveRecord->throwAway) ? "x" : "") << moveRecord->target;
+    char myFlag[2];
+    myFlag[0] = moveRecord->flag;
+    ss << moveRecord->figure << moveRecord->source << ((moveRecord->throwAway) ? "x" : "") << moveRecord->target << ((moveRecord->flag != '\0') ? string(myFlag) : "");
     return ss.str();
     //return this.figure + this.source + ((this.throwAway) ? 'x' : "") + this.target + ((this.flag != '\u0000') ? this.flag : "");
 }
